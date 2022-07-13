f726bad7bcfbb1c788d69927c2a673e2faf5b4bf
nsalei <natashokich67@gmail.com>
2022-07-13 16:58:40 +0400

    feat: add guessing-game solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.number;
+        this.arrayOfNum = [];
+        this.middleNum;
+    }
 
     setRange(min, max) {
-
+        for (var i = min; i <= max; i++) {
+            this.arrayOfNum.push(i);
+        }
     }
 
     guess() {
-
+        this.middleNum = Math.round((this.arrayOfNum.length - 1) / 2);
+        return this.arrayOfNum[this.middleNum];
     }
 
     lower() {
-
+    
+        this.arrayOfNum.splice(this.middleNum + 1, ((this.arrayOfNum.length) - this.middleNum));
     }
 
     greater() {
-
+        this.arrayOfNum.splice(0, this.middleNum);
     }
 }
 
